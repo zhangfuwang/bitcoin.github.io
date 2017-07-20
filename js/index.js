aa906aa05b269da96fa45c489a4b5925331eccdf
zhangfuwang <zhang_xu6941@163.com>
2017-07-20 14:34:12 +0800

    bitcoin

--- js/index.js
+++ js/index.js
@@ -15,9 +15,29 @@
 })(window,undefined);
 $(function(){
    $(" #price button").click(function(event){
-       console.log("1");
+       //console.log("1");
        event.preventDefault();
        $(this).addClass("active").siblings(".active").removeClass("active");
 
    })
+    $(" #jian").click(function(){
+        //console.log("1");
+
+        var btnHtml=$(this).next().html();
+        console.log(btnHtml);
+        if(btnHtml>0){
+            btnHtml--;
+        }
+        $(this).next().html(btnHtml)
+    })
+    $(" #jia").click(function(){
+        //console.log("1");
+
+        var btnHtml=$(this).prev().html();
+        console.log(btnHtml);
+        if(btnHtml>=0){
+            btnHtml++;
+        }
+        $(this).prev().html(btnHtml)
+    })
 });
